f695f44c8ca7ce264ca648aa3a8cc7ab000cedde
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-04-10 15:52:17 +0530

    app created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./styles.css";
+import Inputbox from "./component/Inputbox";
 
 export default function App() {
   return (
     <div className="App">
+      <Inputbox />
       <h1>Hello vikas</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
---
+++ src/component/Inputbox.jsx
@@ -0,0 +1,85 @@
+import { useReducer } from "react";
+
+const initstate = {
+  count: "",
+  count2: "",
+  res: ""
+};
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case "COUNT_ONE":
+      return { ...state, count: payload };
+    case "COUNT_TWO":
+      return { ...state, count2: payload };
+    case "ADD":
+      return { ...state, res: +state.count + +state.count2 };
+    case "SUB":
+      return { ...state, res: +state.count - +state.count2 };
+    case "MULTI":
+      return { ...state, res: +state.count * +state.count2 };
+    case "DIV":
+      return { ...state, res: +state.count / +state.count2 };
+
+    default:
+      return { ...state };
+  }
+};
+
+const Inputbox = () => {
+  const [state, dispatch] = useReducer(reducer, initstate);
+
+  const { res, count, count2 } = state;
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => {
+          dispatch({ type: "COUNT_ONE", payload: e.target.value });
+        }}
+      />{" "}
+      <br />
+      <input
+        type="text"
+        onChange={(e) => {
+          dispatch({ type: "COUNT_TWO", payload: e.target.value });
+        }}
+      />
+      <br />
+      <input type="text" value={res} />
+      <br />
+      <button
+        onClick={() => {
+          dispatch({ type: "ADD" });
+        }}
+      >
+        ADD
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "SUB" });
+        }}
+      >
+        SUB
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "MULTI" });
+        }}
+      >
+        {" "}
+        MULTI
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "DIV" });
+        }}
+      >
+        DIVISION
+      </button>
+    </div>
+  );
+};
+
+export default Inputbox;
